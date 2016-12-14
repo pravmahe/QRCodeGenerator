7918b8f114c5996c09a4091a9388f988753a5727
unknown <pravmahe@PRAVMAHE-28PU1.partnet.cisco.com>
2016-12-14 11:33:43 +0530

    Test Branch Commit.

--- src/com/javapapers/java/QRCode.java
+++ src/com/javapapers/java/QRCode.java
@@ -27,7 +27,7 @@ public class QRCode {
 
 	public static void main(String[] args) throws WriterException, IOException,
 			NotFoundException {
-		String qrCodeData = "Praveen Say Hello Test";
+		String qrCodeData = "Praveen Say Hello Test Test Branch";
 		String filePath = "QRCode.png";
 		String charset = "UTF-8"; // or "ISO-8859-1"
 		Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
